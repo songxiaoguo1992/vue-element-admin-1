c01bbcf4e9e5207fa332b8f4114da76874132525
weiqinl <772863869@qq.com>
2017-10-28 09:22:21 +0800

    初始化项目 v0.2.0

--- src/router/forms.js
+++
@@ -1,8 +0,0 @@
-import BasicNeedForm from '../views/forms/basicNeed.vue'
-
-export default [{
-    path: 'basicneed',
-    name: 'BasicNeedForm',
-    component: BasicNeedForm,
-    meta: ['基本需求表单'],
-}]
